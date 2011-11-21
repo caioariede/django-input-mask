a85c5df1720fc58b33e568975cabee55e9cbeb88
Caio Ariede <caio.ariede@gmail.com>
2011-11-21 14:40:44 -0200

    more suitable license

--- setup.py
+++ setup.py
@@ -2,12 +2,7 @@ from distutils.core import setup
 from distutils.command.install import INSTALL_SCHEMES
 import os
 
-root = os.path.dirname(os.path.abspath(__file__))
-os.chdir(root)
-
-master_file = open(os.path.join(root, ".git", "refs", "heads", "master"))
-VERSION = '0.1.git-' + master_file.read().strip()
-master_file.close()
+VERSION = '1.1'
 
 # Make data go to the right place.
 # http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
@@ -30,7 +25,7 @@ setup(
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Framework :: Django",
