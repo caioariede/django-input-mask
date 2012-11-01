05a8e807148a7c4416dbbc3ba0ead97dcfe2baa3
Caio Ariede <caio.ariede@gmail.com>
2012-11-01 13:38:33 -0200

    bump

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
 from distutils.command.install import INSTALL_SCHEMES
-import os
 
-VERSION = '1.1'
+
+VERSION = '1.2'
+
 
 # Make data go to the right place.
 # http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
