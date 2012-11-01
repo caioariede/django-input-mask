f8b14c5687a48590e46c0838fb3bcae898b39f78
Caio Ariede <caio.ariede@gmail.com>
2012-11-01 12:45:29 -0200

    Change masks to dict

--- input_mask/contrib/localflavor/br/widgets.py
+++ input_mask/contrib/localflavor/br/widgets.py
@@ -2,16 +2,24 @@ from mask_field.widgets import InputMask
 
 
 class BRPhoneNumberInput(InputMask):
-    mask = '{mask:"99-9999-9999"}'
+    mask = {
+        'mask': '99-9999-9999',
+    }
 
 
 class BRZipCodeInput(InputMask):
-    mask = '{mask:"99999-999"}'
+    mask = {
+        'mask': '99999-999',
+    }
 
 
 class BRCPFInput(InputMask):
-    mask = '{mask:"999-999-999-99"}'
+    mask = {
+        'mask': '999-999-999-99',
+    }
 
 
 class BRCNPJInput(InputMask):
-    mask = '{mask:"99.999.999/9999-9"}'
+    mask = {
+        'mask': '99.999.999/9999-9',
+    }
--- input_mask/contrib/localflavor/us/widgets.py
+++ input_mask/contrib/localflavor/us/widgets.py
@@ -2,12 +2,18 @@ from mask_field.widgets import InputMask
 
 
 class USPhoneNumberInput(InputMask):
-    mask = '{mask:"999-999-9999"}'
+    mask = {
+        'mask': '999-999-9999',
+    }
 
 
 class USSocialSecurityNumberInput(InputMask):
-    mask = '{mask:"999-99-9999"}'
+    mask = {
+        'mask': '999-99-9999',
+    }
 
 
 class BRZipCodeInput(InputMask):
-    mask = '{mask:"99999-9999"}'
+    mask = {
+        'mask': '99999-9999',
+    }
