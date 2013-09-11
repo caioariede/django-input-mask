761c9e2a4e5d9df39f447b4defab32045d6af101
Fabio C. Barrionuevo da Luz <bnafta@gmail.com>
2013-09-11 18:04:50 -0300

    Fixed setup.py to correct install static directory of this app Move static files from input_mask/static/js/ to input_mask/static/input_mask/js/

--- input_mask/widgets.py
+++ input_mask/widgets.py
@@ -39,9 +39,9 @@ class InputMask(forms.TextInput):
         return super(InputMask, self).render(name, value, attrs=attrs)
 
     class Media:
-        js = (settings.STATIC_URL + 'js/jquery.metadata.js',
-              settings.STATIC_URL + 'js/jquery.meio.mask.min.js',
-              settings.STATIC_URL + 'js/text_input_mask.js',)
+        js = (settings.STATIC_URL + 'input_mask/js/jquery.metadata.js',
+              settings.STATIC_URL + 'input_mask/js/jquery.meio.mask.min.js',
+              settings.STATIC_URL + 'input_mask/js/text_input_mask.js',)
 
 
 class DecimalInputMask(InputMask):
