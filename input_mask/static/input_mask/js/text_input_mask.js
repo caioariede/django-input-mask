7bbf10a8b1af98ada47c138d2383b032f2d02b85
Caio Ariede <caio.ariede@gmail.com>
2016-06-03 18:11:52 -0300

    Update JS to be a jQuery plugin

--- input_mask/static/input_mask/js/text_input_mask.js
+++ input_mask/static/input_mask/js/text_input_mask.js
@@ -1,22 +1,51 @@
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;( function( $, window, document, undefined ) {
+
+    "use strict";
+
+    var pluginName = 'djangoInputMask';
+
+    function Plugin(container, options) {
+        this.container = container;
+        this.init();
+    }
+
+    $.extend(Plugin.prototype, {
+        init: function() {
+            $(this.container).find('[data-input-mask]').each(this.initInputMask);
+            $(this.container).find('[data-money-mask]').each(this.initMoneyMask);
+        },
+
+        initInputMask: function() {
+            var input = $(this),
+                opts = input.attr('data-input-mask').replace(/&quot;/g, '"'),
+                opts = JSON.parse(opts),
+                mask = opts.mask;
+
+            opts.placeholder = input.attr('placeholder');
+
+            input.mask(mask, opts);
+        },
+
+        initMoneyMask: function() {
+            var input = $(this),
+                opts = input.attr('data-money-mask').replace(/&quot;/g, '"'),
+                opts = JSON.parse(opts);
+
+            input.maskMoney(opts).maskMoney('mask');
+        }
+    });
+
+    $.fn[pluginName] = function(options) {
+        return this.each(function(){
+            new Plugin(this, options);
+        });
+    };
+
+} )( jQuery, window, document );
+
+
 $(function(){
-    $('[data-input-mask]').each(function(){
-        var input = $(this),
-            opts = input.attr('data-input-mask').replace(/&quot;/g, '"'),
-            opts = JSON.parse(opts),
-            mask = opts.mask;
-
-        delete opts.mask;
-
-        opts.placeholder = input.attr('placeholder');
-
-        $(this).mask(mask, opts);
-    });
-
-    $('[data-money-mask]').each(function(){
-        var input = $(this),
-            opts = input.attr('data-money-mask').replace(/&quot;/g, '"'),
-            opts = JSON.parse(opts);
-
-        $(this).maskMoney(opts).maskMoney('mask');
-    });
+    $('body').djangoInputMask();
 });
